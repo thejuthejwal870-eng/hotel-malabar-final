a83bf308182c0bb85723d5a3c094453f4b92a127
thejuthejwal870-eng <thejuthejwal870@gmail.com>
2026-09-20 20:35:04 +0530

    Add native background alert app files

---
+++ android-admin-alerts/app/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "in.malabarhotel.adminalerts"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "in.malabarhotel.adminalerts"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
+kotlin { jvmToolchain(17) }
+dependencies { implementation("androidx.core:core-ktx:1.15.0") }
